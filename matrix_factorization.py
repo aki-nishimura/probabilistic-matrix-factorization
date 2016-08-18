Bug fix: error in the calculation of logp.

matrix_factorization.py
@@ -54,7 +54,7 @@ class MatrixFactorization(object):
         # This function computes the log posterior probability (with the weight
         # parameter marginalized out).
         loglik = - (self.prior_param['df'] + 1) / 2 * np.sum(
-            np.log( 1 + (self.y_coo.data - mu) ** 2 * self.prior_param['weight'] / self.prior_param['df'])
+            np.log( 1 + (self.y_coo.data - mu) ** 2 * self.prior_param['weight'] * psi / self.prior_param['df'])
         )
         logp_prior = \
             - self.prior_param['col_bias_scale'] ** -2 / 2 * np.sum(c ** 2) + \